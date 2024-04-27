5c371a0389745afd3a14f5194f81c6053b38f4ce
White, Otto <otto.white20@imperial.ac.uk>
2024-04-27 14:42:25 +0100

    Add lockfile, zsh

--- flake.nix
+++ flake.nix
@@ -17,27 +17,38 @@
     #     # Add necessary packages here
     #   ];
     # };
+
+    # This is another flake that we're importing, can do this arbitrarily with flakes
     flake-utils.url = "github:numtide/flake-utils";
   };
 
+  # These are the inputs that generate the outputs
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+
+	# Get the correct packages for each of the architectures we're building for
+	# If we didn't 
         pkgs = import nixpkgs {
           inherit system;
         };
       in {
+	# Default shell that launches when we write nix develop (devShells -> nix develop)
         devShells.default = pkgs.mkShell {
 
-                    # Add buildInputs here i.e packages you want to be available in the dev shell
-          buildInputs = (with pkgs; [
-                        # Add packages here e.g
-                        cowsay
+          # Add buildInputs here i.e packages you want to be available in the dev shell
+          packages = (with pkgs; [
+            zsh
           ]);
-                    #  whatever you want to run when entering a dev shell
-                    shellHook = ''
-                    cowsay "Hello Otto"
-                    '';
+	  # Goes for the cowsay package in nix packages
+          # buildInputs = [
+          #               pkgs.cowsay
+          # ];
+
+          #  whatever you want to run when entering a dev shell
+          shellHook = ''
+            exec zsh
+          '';
         };
       }
     );
