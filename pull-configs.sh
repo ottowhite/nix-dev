4b600dc7ba2a0501b32f60d3ccbd0b0610238fa8
White, Otto <otto.white20@imperial.ac.uk>
2025-06-01 12:46:35 +0100

    pull cursor configs into cursor folder

--- pull-configs.sh
+++ pull-configs.sh
@@ -1,2 +1,2 @@
-cp /Users/ottowhite/Library/Application\ Support/Cursor/User/keybindings.json .
-cp /Users/ottowhite/Library/Application\ Support/Cursor/User/settings.json .
+cp /Users/ottowhite/Library/Application\ Support/Cursor/User/keybindings.json cursor/
+cp /Users/ottowhite/Library/Application\ Support/Cursor/User/settings.json cursor/
