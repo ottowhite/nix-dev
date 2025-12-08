41b396bdc2028f9993036ad4d8e2eb48559d6182
Otto White <whiteotto4@gmail.com>
2025-12-08 09:09:48 +0000

    nixos: permanently enable nix-command and flakes

--- configuration.nix
+++ configuration.nix
@@ -109,6 +109,7 @@
     zsh
   ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
