60ff48a3267156fcd77732f9d56e01bbf16e2782
Otto White <whiteotto4@gmail.com>
2025-12-08 02:32:10 +0000

    nix: change vim to neovim in config.nix

--- configuration.nix
+++ configuration.nix
@@ -103,7 +103,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim
+    neovim
     git
     wget
     zsh
