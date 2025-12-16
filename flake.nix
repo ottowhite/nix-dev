29f9aaaf294e284196d651a096ba6724bd8a9a23
White, Otto <otto.white20@imperial.ac.uk>
2025-12-16 19:53:04 +0100

    nix: add comment for the rendering fix in my flake

--- flake.nix
+++ flake.nix
@@ -165,6 +165,7 @@
             copy_and_own init.vim   ${nix-dev-deps}     ~/.config/nvim
 
             export SHELL=${pkgs.zsh}/bin/zsh
+            # NOTE: Fixes rendering issues on macOS for some reason: https://github.com/NixOS/nixpkgs/issues/376958
 	    unset DEVELOPER_DIR
             exec zsh
           '';
