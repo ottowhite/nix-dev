0e11da9ac4b13bd3fe489cbfe79fd7c541cba160
Otto White (ow20@imperial.ac.uk) <ow20@komodo01.doc.res.ic.ac.uk>
2026-02-04 12:50:53 +0000

    feat(otstack): add authentication and repo listing of github

--- otstack/main.py
+++ otstack/main.py
@@ -1,3 +1,18 @@
+from github import Github
+from github import Auth
+from dotenv import load_dotenv
+import os
 
 if __name__ == "__main__":
-    print("Hello world")
+    load_dotenv()
+    access_token: str | None = os.environ.get("GITHUB_PERSONAL_ACCESS_TOKEN", None)
+
+    if access_token is None:
+        print("Need to set GitHub personal access token in .env")
+        exit(-1)
+    else:
+        auth = Auth.Token(access_token)
+        github_client = Github(auth=auth)
+        for repo in github_client.get_user().get_repos():
+            print(repo.name)
+        github_client.close()
