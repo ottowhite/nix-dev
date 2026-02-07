955b8910a8acc47a6302f3f2f0d13f810e7093d6
Otto White <whiteotto4@gmail.com>
2026-02-07 17:48:39 +0000

    nixos: install libreoffice and enable bluetooth

--- configuration.nix
+++ configuration.nix
@@ -51,6 +51,27 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   # Useful config here: https://discourse.nixos.org/t/hardware-nvidia-open-is-used-but-not-defined-error-when-updating-nixos-flake-config/51359/10
   hardware.nvidia.open = false;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
 
 
 
@@ -120,6 +141,7 @@
     vscode.fhs
     cudatoolkit
     tailscale
+    libreoffice-qt
   ];
 
   nix.settings = {
